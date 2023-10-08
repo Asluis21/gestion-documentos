412d3c66497e21eb4e4168f4b0b433c720a28e03
Asluis21 <luis_alvarado1225@outlook.com>
2023-10-07 19:24:17 -0500

    commit 1

---
+++ spring-boot-gestion-documentos-api/src/main/java/com/api/springboot/app/SpringBootGestionDocumentosApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGestionDocumentosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGestionDocumentosApiApplication.class, args);
+	}
+
+}
---
+++ spring-boot-gestion-documentos-api/src/test/java/com/api/springboot/app/SpringBootGestionDocumentosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGestionDocumentosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
